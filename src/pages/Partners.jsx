120002d825d558a7b0f82f28a503a0429fb1fd09
david <loopesdavid@gmail.com>
2026-05-16 16:47:59 -0300

    Criando a pagina Parcerias

---
+++ src/pages/Partners.jsx
@@ -0,0 +1,33 @@
+function Partners() {
+  return (
+    <section className="page-container">
+
+      <h1>Parcerias</h1>
+
+      <p>
+        Trabalhamos lado a lado com empresas
+        que compartilham nossa visão de inovação.
+      </p>
+
+      <div className="partner-card">
+
+        <div className="partner-image">
+          {/* IMAGEM DA EMPRESA PARCEIRA */}
+        </div>
+
+        <div>
+          <h2>Empresa Parceira</h2>
+
+          <p>
+            Aqui você poderá colocar a descrição
+            da parceria e os projetos realizados.
+          </p>
+        </div>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Partners

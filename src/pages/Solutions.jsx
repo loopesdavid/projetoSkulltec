cbcbd20959ccd0ebc55b0a5e4d8944dec141f4b3
david <loopesdavid@gmail.com>
2026-05-16 16:47:04 -0300

    Criando a pagina soluções

---
+++ src/pages/Solutions.jsx
@@ -0,0 +1,21 @@
+function Solutions() {
+  return (
+    <section className="page-container">
+
+      <h1>Soluções</h1>
+
+      <p>
+        Desenvolvemos sistemas modernos,
+        aplicações web, automações e
+        soluções digitais para empresas.
+      </p>
+
+      <div className="system-preview">
+        {/* PRINT DO SISTEMA AQUI */}
+      </div>
+
+    </section>
+  )
+}
+
+export default Solutions
